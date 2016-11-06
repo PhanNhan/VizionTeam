451181bdf37363b984f3982b92cfa7efdd2dcc8c
Nhan Phan <Nhan Phan>
2016-11-06 22:21:41 +0700

    Init project

---
+++ Assets/Script/AppController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AppController : MonoBehaviour {
+
+    [SerializeField] private GameObject imageTarget;
+    private Vector3[] sizeOfObject;
+
+    void Start() {
+        sizeOfObject = new Vector3[4];
+        sizeOfObject[0] = new Vector3(4f, 4f, 2f);
+        sizeOfObject[1] = new Vector3(0.0075f, 0.0075f, 0.0075f);
+        sizeOfObject[2] = new Vector3(0.03f, 0.03f, 0.03f);
+        sizeOfObject[3] = new Vector3(0.0025f, 0.0025f, 0.0025f);
+
+    }
+
+    public void chooseObject() {
+
+        DestroyChildObject();
+        string nameChoose = UIButton.current.name;
+        GameObject objectChoose = Instantiate(Resources.Load("Prefabs/" + nameChoose)) as GameObject;
+        objectChoose.transform.SetParent(imageTarget.transform);
+        objectChoose.transform.localPosition = Vector3.zero;
+        objectChoose.transform.localScale = SizeOfObject(nameChoose);
+
+    }
+
+    void DestroyChildObject()
+    {
+        int childCount = imageTarget.transform.childCount;
+        Debug.Log(childCount);
+        if (childCount >=1)
+        {
+            for(int i=0; i < childCount; i++ )
+                Destroy(imageTarget.transform.GetChild(i).gameObject);
+        }  
+    }
+
+    Vector3 SizeOfObject(string nameObject) {
+        switch (nameObject) {
+            case "Clock":
+                return sizeOfObject[0];
+            case "Sofa":
+                return sizeOfObject[1];              
+            case "TVStand":
+                return sizeOfObject[2];
+            case "Bed":
+                return sizeOfObject[3];
+        }
+        return Vector3.one;
+    }
+}
